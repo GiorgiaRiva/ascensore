1b9ca7a4ea7044be27962c5ecfc357b2ad6ffbea
RivaGiorgia <riva.giorgia@itisgrassi.edu.it>
2025-03-21 10:37:46 +0100

    inizio

---
+++ Ascensore.java
@@ -0,0 +1,54 @@
+class Ascensore {
+    private int PianoCorrente;
+    private int CapienzaMassima;
+    public Boolean PorteAperte;
+    //arraylista
+
+
+    public Ascensore(int PianoCorrente, int CapienzaMassima, Boolean PorteAperte) {
+        this.PianoCorrente = PianoCorrente;
+        this.CapienzaMassima = CapienzaMassima;
+        this.PorteAperte = false;
+    //arraylista
+    }
+
+
+    public void Aprileporte(ascensore a) {
+       
+    }
+
+
+     public void Chiudileporte(ascensore a) {
+       
+    }
+
+
+
+
+    public void AggiungiPersona(ascensore a) {
+       
+        }
+
+
+     public void RimuoviPersone(ascensore a) {
+       
+    }
+
+
+     public void Discesa(ascensore a) {
+       
+    }
+   
+    public void Salita(ascensore a) {
+       
+    }
+
+
+
+
+    }
+
+
+public String toString(){
+    return String id + int destinazione;
+}
---
+++ Persona.java
@@ -0,0 +1,28 @@
+
+class Persona {
+    private String id;
+    private int destinazione;
+
+
+    public Persona(String id, int destinazione) {
+        this.id = id;
+        this.destinazione = destinazione;
+    }
+
+
+    public void Saliascensore(ascensore a) {
+       
+    }
+
+
+    public void Scendiascensore(ascensore a) {
+       
+        }
+    }
+
+
+public String toString(){
+    return String id + int destinazione;
+}
+
+
